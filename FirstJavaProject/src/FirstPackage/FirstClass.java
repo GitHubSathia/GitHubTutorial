4a778a5431ae1950072540acea5a06944fdf7a67
pappr <pappr@LAPTOP-GMG8JSNM.fios-router.home>
2020-01-26 17:09:31 -0500

    change to firstclass.java

---
+++ FirstJavaProject/src/FirstPackage/FirstClass.java
@@ -0,0 +1,13 @@
+package FirstPackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1/0");
+		System.out.println("Hi there");
+		String s="";
+		System.out.println(s.toString().length());
+	}
+
+}
